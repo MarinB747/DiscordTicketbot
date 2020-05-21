68bfa0678c6d7faf8f66743369403110047ac6d2
MarinB747 <62120430+MarinB747@users.noreply.github.com>
2020-05-21 19:12:39 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,441 @@
+import discord
+from discord import raw_models
+from discord import message
+from discord.ext import commands
+from discord.ext import tasks
+from discord.ext.commands import has_permissions, MissingPermissions, context
+import json
+import asyncio
+import discord
+
+
+bot = commands.Bot(command_prefix=".")
+bot.remove_command("help")
+client = commands.Bot(command_prefix=".")
+
+@bot.event
+async def on_ready():
+    print("Bot running with:")
+    print("Username: ", bot.user.name)
+    print("User ID: ", bot.user.id)
+
+@bot.command()
+async def help(ctx):
+    with open("data.json") as f:
+        data = json.load(f)
+    
+    valid_user = False
+
+    for role_id in data["verified-roles"]:
+        try:
+            if ctx.guild.get_role(role_id) in ctx.author.roles:
+                valid_user = True
+        except:
+            pass
+    
+    if ctx.author.guild_permissions.administrator or valid_user:
+
+        em = discord.Embed(title="TicketsKobasicom Help", description="", color=0x00a8ff)
+        em.add_field(name="`.prijava <message>`", value="This creates a new ticket. Add any words after the command if you'd like to send a message when we initially create your ticket.")
+        em.add_field(name="`.close`", value="Use this to close a ticket. This command only works in ticket channels.")
+        em.add_field(name="`.addaccess <role_id>`", value="This can be used to give a specific role access to all tickets. This command can only be run if you have an admin-level role for this bot.")
+        em.add_field(name="`.delaccess <role_id>`", value="This can be used to remove a specific role's access to all tickets. This command can only be run if you have an admin-level role for this bot.")
+        em.add_field(name="`.addpingedrole <role_id>`", value="This command adds a role to the list of roles that are pinged when a new ticket is created. This command can only be run if you have an admin-level role for this bot.")
+        em.add_field(name="`.delpingedrole <role_id>`", value="This command removes a role from the list of roles that are pinged when a new ticket is created. This command can only be run if you have an admin-level role for this bot.")
+        em.add_field(name="`.addadminrole <role_id>`", value="This command gives all users with a specific role access to the admin-level commands for the bot, such as `.addpingedrole` and `.addaccess`. This command can only be run by users who have administrator permissions for the entire server.")
+        em.add_field(name="`.deladminrole <role_id>`", value="This command removes access for all users with the specified role to the admin-level commands for the bot, such as `.addpingedrole` and `.addaccess`. This command can only be run by users who have administrator permissions for the entire server.")
+        em.set_footer(text="Kobaja")
+
+        await ctx.send(embed=em)
+    
+    else:
+
+        em = discord.Embed(title = "TicketsKobasicom Help", description ="", color = 0x00a8ff)
+        em.add_field(name="`.prijava <message>`", value="This creates a new ticket. Add any words after the command if you'd like to send a message when we initially create your ticket.")
+        em.add_field(name="`.close`", value="Use this to close a ticket. This command only works in ticket channels.")
+        em.set_footer(text="Auroris Development")
+
+        await ctx.send(embed=em)
+
+
+@bot.command()
+async def info(ctx):
+    with open("data.json") as f:
+        data = json.load(f)
+
+    valid_user = False
+
+    for role_id in data["verified-roles"]:
+        try:
+            if ctx.guild.get_role(role_id) in ctx.author.roles:
+                valid_user = True
+        except:
+            pass
+
+    if ctx.author.guild_permissions.administrator or valid_user:
+
+        em = discord.Embed(title="TicketsKobasicom Info", description="", color=0x00a8ff)
+        em.add_field(name="Dobrodosli",
+                     value="Dobrodosli u mirnu plovidbu, discord EU WoW guilda Grah s Kobasicom, prijavite se komandom .prijava <tu se predstavite, naipiste kako ste culi za nas, odakle ste dosli, koji frend vas je zvao i slicno> ")
+        em.set_footer(text="Kobaja")
+
+        await ctx.send(embed=em)
+
+    else:
+
+        em = discord.Embed(title="TicketsKobasicom Help", description="", color=0x00a8ff)
+        em.add_field(name="`.prijava <message>`",
+                     value="This creates a new ticket. Add any words after the command if you'd like to send a message when we initially create your ticket.")
+        em.add_field(name="`.close`", value="Use this to close a ticket. This command only works in ticket channels.")
+        em.set_footer(text="Auroris Development")
+
+        await ctx.send(embed=em)
+
+
+@bot.command(pass_context=True)
+@commands.has_permissions(administrator=True)
+async def clean(ctx, limit: int):
+        await ctx.channel.purge(limit=limit)
+
+
+@clean.error
+async def clear_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You cant do that!")
+
+
+@bot.command()
+async def prijava(ctx, *, args = None):
+
+    await bot.wait_until_ready()
+
+    if args == None:
+        message_content = "Please wait, we will be with you shortly!"
+    
+    else:
+        message_content = "".join(args)
+
+    with open("data.json") as f:
+        data = json.load(f)
+
+    ticket_number = int(data["ticket-counter"])
+    ticket_number += 1
+
+    ticket_channel = await ctx.guild.create_text_channel("ticket-{}".format(ticket_number))
+    await ticket_channel.set_permissions(ctx.guild.get_role(ctx.guild.id), send_messages=False, read_messages=False)
+
+    for role_id in data["valid-roles"]:
+        role = ctx.guild.get_role(role_id)
+
+        await ticket_channel.set_permissions(role, send_messages=True, read_messages=True, add_reactions=True, embed_links=True, attach_files=True, read_message_history=True, external_emojis=True)
+    
+    await ticket_channel.set_permissions(ctx.author, send_messages=True, read_messages=True, add_reactions=True, embed_links=True, attach_files=True, read_message_history=True, external_emojis=True)
+
+    em = discord.Embed(title="New ticket from {}#{}".format(ctx.author.name, ctx.author.discriminator), description= "{}".format(message_content), color=0x00a8ff)
+
+    await ticket_channel.send(embed=em)
+
+    pinged_msg_content = ""
+    non_mentionable_roles = []
+
+    if data["pinged-roles"] != []:
+
+        for role_id in data["pinged-roles"]:
+            role = ctx.guild.get_role(role_id)
+
+            pinged_msg_content += role.mention
+            pinged_msg_content += " "
+
+            if role.mentionable:
+                pass
+            else:
+                await role.edit(mentionable=True)
+                non_mentionable_roles.append(role)
+        
+        await ticket_channel.send(pinged_msg_content)
+
+        for role in non_mentionable_roles:
+            await role.edit(mentionable=False)
+    
+    data["ticket-channel-ids"].append(ticket_channel.id)
+
+    data["ticket-counter"] = int(ticket_number)
+    with open("data.json", 'w') as f:
+        json.dump(data, f)
+
+
+    created_em = discord.Embed(title="Ticket s Kobasicom", description="Ticket poslan, neko od uprave se javi cim vidi, pozz. ".format(ticket_channel.mention), color=0x00a8ff)
+    await clean(ctx, 1)
+
+
+@bot.command()
+async def close(ctx):
+    with open('data.json') as f:
+        data = json.load(f)
+
+    if ctx.channel.id in data["ticket-channel-ids"]:
+
+        channel_id = ctx.channel.id
+    await ctx.channel.delete()
+
+
+
+
+
+@bot.command()
+async def addaccess(ctx, role_id=None):
+
+    with open('data.json') as f:
+        data = json.load(f)
+    
+    valid_user = False
+
+    for role_id in data["verified-roles"]:
+        try:
+            if ctx.guild.get_role(role_id) in ctx.author.roles:
+                valid_user = True
+        except:
+            pass
+    
+    if valid_user or ctx.author.guild_permissions.administrator:
+        role_id = int(role_id)
+
+        if role_id not in data["valid-roles"]:
+
+            try:
+                role = ctx.guild.get_role(role_id)
+
+                with open("data.json") as f:
+                    data = json.load(f)
+
+                data["valid-roles"].append(role_id)
+
+                with open('data.json', 'w') as f:
+                    json.dump(data, f)
+                
+                em = discord.Embed(title="Auroris Tickets", description="You have successfully added `{}` to the list of roles with access to tickets.".format(role.name), color=0x00a8ff)
+
+                await ctx.send(embed=em)
+
+            except:
+                em = discord.Embed(title="Auroris Tickets", description="That isn't a valid role ID. Please try again with a valid role ID.")
+                await ctx.send(embed=em)
+        
+        else:
+            em = discord.Embed(title="Auroris Tickets", description="That role already has access to tickets!", color=0x00a8ff)
+            await ctx.send(embed=em)
+    
+    else:
+        em = discord.Embed(title="Auroris Tickets", description="Sorry, you don't have permission to run that command.", color=0x00a8ff)
+        await ctx.send(embed=em)
+
+@bot.command()
+async def delaccess(ctx, role_id=None):
+    with open('data.json') as f:
+        data = json.load(f)
+    
+    valid_user = False
+
+    for role_id in data["verified-roles"]:
+        try:
+            if ctx.guild.get_role(role_id) in ctx.author.roles:
+                valid_user = True
+        except:
+            pass
+
+    if valid_user or ctx.author.guild_permissions.administrator:
+
+        try:
+            role_id = int(role_id)
+            role = ctx.guild.get_role(role_id)
+
+            with open("data.json") as f:
+                data = json.load(f)
+
+            valid_roles = data["valid-roles"]
+
+            if role_id in valid_roles:
+                index = valid_roles.index(role_id)
+
+                del valid_roles[index]
+
+                data["valid-roles"] = valid_roles
+
+                with open('data.json', 'w') as f:
+                    json.dump(data, f)
+
+                em = discord.Embed(title="Auroris Tickets", description="You have successfully removed `{}` from the list of roles with access to tickets.".format(role.name), color=0x00a8ff)
+
+                await ctx.send(embed=em)
+            
+            else:
+                
+                em = discord.Embed(title="Auroris Tickets", description="That role already doesn't have access to tickets!", color=0x00a8ff)
+                await ctx.send(embed=em)
+
+        except:
+            em = discord.Embed(title="Auroris Tickets", description="That isn't a valid role ID. Please try again with a valid role ID.")
+            await ctx.send(embed=em)
+    
+    else:
+        em = discord.Embed(title="Auroris Tickets", description="Sorry, you don't have permission to run that command.", color=0x00a8ff)
+        await ctx.send(embed=em)
+
+@bot.command()
+async def addpingedrole(ctx, role_id=None):
+
+    with open('data.json') as f:
+        data = json.load(f)
+    
+    valid_user = False
+
+    for role_id in data["verified-roles"]:
+        try:
+            if ctx.guild.get_role(role_id) in ctx.author.roles:
+                valid_user = True
+        except:
+            pass
+    
+    if valid_user or ctx.author.guild_permissions.administrator:
+
+        role_id = int(role_id)
+
+        if role_id not in data["pinged-roles"]:
+
+            try:
+                role = ctx.guild.get_role(role_id)
+
+                with open("data.json") as f:
+                    data = json.load(f)
+
+                data["pinged-roles"].append(role_id)
+
+                with open('data.json', 'w') as f:
+                    json.dump(data, f)
+
+                em = discord.Embed(title="Auroris Tickets", description="You have successfully added `{}` to the list of roles that get pinged when new tickets are created!".format(role.name), color=0x00a8ff)
+
+                await ctx.send(embed=em)
+
+            except:
+                em = discord.Embed(title="Auroris Tickets", description="That isn't a valid role ID. Please try again with a valid role ID.")
+                await ctx.send(embed=em)
+            
+        else:
+            em = discord.Embed(title="Auroris Tickets", description="That role already receives pings when tickets are created.", color=0x00a8ff)
+            await ctx.send(embed=em)
+    
+    else:
+        em = discord.Embed(title="Auroris Tickets", description="Sorry, you don't have permission to run that command.", color=0x00a8ff)
+        await ctx.send(embed=em)
+
+@bot.command()
+async def delpingedrole(ctx, role_id=None):
+
+    with open('data.json') as f:
+        data = json.load(f)
+    
+    valid_user = False
+
+    for role_id in data["verified-roles"]:
+        try:
+            if ctx.guild.get_role(role_id) in ctx.author.roles:
+                valid_user = True
+        except:
+            pass
+    
+    if valid_user or ctx.author.guild_permissions.administrator:
+
+        try:
+            role_id = int(role_id)
+            role = ctx.guild.get_role(role_id)
+
+            with open("data.json") as f:
+                data = json.load(f)
+
+            pinged_roles = data["pinged-roles"]
+
+            if role_id in pinged_roles:
+                index = pinged_roles.index(role_id)
+
+                del pinged_roles[index]
+
+                data["pinged-roles"] = pinged_roles
+
+                with open('data.json', 'w') as f:
+                    json.dump(data, f)
+
+                em = discord.Embed(title="Auroris Tickets", description="You have successfully removed `{}` from the list of roles that get pinged when new tickets are created.".format(role.name), color=0x00a8ff)
+                await ctx.send(embed=em)
+            
+            else:
+                em = discord.Embed(title="Auroris Tickets", description="That role already isn't getting pinged when new tickets are created!", color=0x00a8ff)
+                await ctx.send(embed=em)
+
+        except:
+            em = discord.Embed(title="Auroris Tickets", description="That isn't a valid role ID. Please try again with a valid role ID.")
+            await ctx.send(embed=em)
+    
+    else:
+        em = discord.Embed(title="Auroris Tickets", description="Sorry, you don't have permission to run that command.", color=0x00a8ff)
+        await ctx.send(embed=em)
+
+
+@bot.command()
+@has_permissions(administrator=True)
+async def addadminrole(ctx, role_id=None):
+
+    try:
+        role_id = int(role_id)
+        role = ctx.guild.get_role(role_id)
+
+        with open("data.json") as f:
+            data = json.load(f)
+
+        data["verified-roles"].append(role_id)
+
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
+        
+        em = discord.Embed(title="Auroris Tickets", description="You have successfully added `{}` to the list of roles that can run admin-level commands!".format(role.name), color=0x00a8ff)
+        await ctx.send(embed=em)
+
+    except:
+        em = discord.Embed(title="Auroris Tickets", description="That isn't a valid role ID. Please try again with a valid role ID.")
+        await ctx.send(embed=em)
+
+@bot.command()
+@has_permissions(administrator=True)
+async def deladminrole(ctx, role_id=None):
+    try:
+        role_id = int(role_id)
+        role = ctx.guild.get_role(role_id)
+
+        with open("data.json") as f:
+            data = json.load(f)
+
+        admin_roles = data["verified-roles"]
+
+        if role_id in admin_roles:
+            index = admin_roles.index(role_id)
+
+            del admin_roles[index]
+
+            data["verified-roles"] = admin_roles
+
+            with open('data.json', 'w') as f:
+                json.dump(data, f)
+            
+            em = discord.Embed(title="Auroris Tickets", description="You have successfully removed `{}` from the list of roles that get pinged when new tickets are created.".format(role.name), color=0x00a8ff)
+
+            await ctx.send(embed=em)
+        
+        else:
+            em = discord.Embed(title="Auroris Tickets", description="That role isn't getting pinged when new tickets are created!", color=0x00a8ff)
+            await ctx.send(embed=em)
+
+    except:
+        em = discord.Embed(title="Auroris Tickets", description="That isn't a valid role ID. Please try again with a valid role ID.")
+        await ctx.send(embed=em)
+
+bot.run("NjkyODM5MjYwNTI4NzA1NjQ2.Xn0YQA.cgT2Cr0VGKq0HWt1zQY6-qbV_cQ")
